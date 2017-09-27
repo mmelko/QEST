1f8056bb2cfdf166aab64e1792bd2ff86f8c10a3
Matej Melko <mmelko@redhat.com>
2017-09-27 13:19:11 +0200

    [server] modify DB conf to running on local or openshift fix NPE when username is not filled

--- server/config/db.js
+++ server/config/db.js
@@ -3,18 +3,26 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var USER = process.env.MONGODB_USER;
-var PASSWORD = process.env.MONGODB_PASSWORD;
 
-var serviceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
-var HOST = process.env[serviceName+"_SERVICE_HOST"]+":"+process.env[serviceName+"_SERVICE_PORT"];
-console.log("==========>"+HOST);
-var DATABASE = process.env.MONGODB_DATABASE; 
-var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL
+var mongoConnectionString = 'mongodb://localhost/qest';
 
-console.log(USER + " "+PASSWORD+ " "+serviceName+ " "+DATABASE+ " "+mongoURL)
-var mongoConnectionString = 'mongodb://'+USER+':'+PASSWORD+"@"+HOST+'/'+DATABASE;
+if (process.env.DATABASE_SERVICE_NAME) {
+  var USER = process.env.MONGODB_USER;
+  var PASSWORD = process.env.MONGODB_PASSWORD;
+
+  var serviceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
+  var HOST = process.env[serviceName + "_SERVICE_HOST"] + ":" + process.env[serviceName + "_SERVICE_PORT"];
+
+  var DATABASE = process.env.MONGODB_DATABASE;
+  var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL
+
+  var mongoConnectionString = 'mongodb://' + USER + ':' + PASSWORD + "@" + HOST + '/' + DATABASE;
+  console.log("Openshift deployment");
+}
+else console.log("Local deployment");
+
+
 module.exports = {
-    //url : 'mongodb://localhost/qest'
-    url: mongoConnectionString
+  //url : 'mongodb://localhost/qest'
+  url: mongoConnectionString
 };
